3ee34444dae628b40495e91b2393b074e6b224ba
David Andersson <lejonmanen@gmail.com>
2022-01-19 09:57:16 +0100

    Final test case, calculator

--- src/components/Calculator.test.tsx
+++ src/components/Calculator.test.tsx
@@ -43,6 +43,31 @@ describe('Calculator component', () => {
 		expect(element).toBeInTheDocument()
 	})
 
+	it('displays the value 4 when the user clicks on +1 thrice', () => {
+		render( <Calculator /> )
+		const button = screen.getByRole('button', { name: '+1' })
+
+		userEvent.click(button)
+		userEvent.click(button)
+		userEvent.click(button)
+
+		const element = screen.getByText(/Value: 4/i)
+		expect(element).toBeInTheDocument()
+	})
+
+	// Bonusmaterial: reguljära uttryck
+	// /cars*/  - carssssss
+	// /cars?/  - car, cars
+	// /car./   - card, car", ...
+	// /car[ds]/ - card, cars
+	// /car[ds]* - car, card, cars, cards, carsssdddssdds
+	// ^ - början av sträng
+	// $ - slutet av sträng
+	// /^car/
+	// /car\*/ - car*
+
+	// " citattecken \" som inte bryter strängen "
+
 	it('displays the value 0 when user clicks on Clear', () => {
 		render( <Calculator /> )
 		const button = screen.getByRole('button', { name: 'Clear' })
